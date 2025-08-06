c14abd7f81f79b176a1cf0568a74a95e8365b0b3
Bobojon <157896950+bobojonabdurahmonov@users.noreply.github.com>
2025-08-06 16:10:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount('/static', StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def entry(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
